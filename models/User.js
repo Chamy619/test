08bb7a73c9e1460e6ce7a065949b230985be4167
Chamy619 <yd95069506@naver.com>
2021-05-14 22:12:04 +0900

    bycrypt로 비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -25,6 +27,28 @@ const userSchema = mongoose.Schema({
     image: String,
     tocken: String,
     tockenExp: Number
+});
+
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        // 비밀번호를 암호화
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) {
+                return next(err);
+            }
+
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) {
+                    return next(err);
+                }
+
+                user.password = hash;
+                next();
+            });
+        });
+    }
 });
 
 const User = mongoose.model('User', userSchema);
